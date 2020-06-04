9e8866ac583a0e391fde45e27d26546efd363b13
Manfred Riem <mriem@manorrock.com>
2020-06-04 17:10:28 -0600

    Fixes issue #20 - Remove replication plugin

--- src/main/java/se/unlogic/eagledns/plugins/zonereplicator/ReplicationException.java
+++
@@ -1,8 +0,0 @@
-package se.unlogic.eagledns.plugins.zonereplicator;
-
-
-public class ReplicationException extends Exception {
-
-	private static final long serialVersionUID = -7036780473719322510L;
-
-}
