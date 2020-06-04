a2b816a311669603e405fbdb3dd6130a7d078716
Manfred Riem <mriem@manorrock.com>
2020-06-04 16:20:46 -0600

    Fixes issue #19 - Remove remote management plugin

--- src/main/java/se/unlogic/eagledns/ZoneChangeCallback.java
+++ src/main/java/se/unlogic/eagledns/ZoneChangeCallback.java
@@ -7,7 +7,6 @@
  ******************************************************************************/
 package se.unlogic.eagledns;
 
-import se.unlogic.eagledns.plugins.remotemanagement.EagleManager;
 import se.unlogic.eagledns.zoneproviders.ZoneProvider;
 
 /**
